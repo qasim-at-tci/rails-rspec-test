d47a9743feb60c689cfd68a3c0bfeb88c103d69b
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2021-12-14 00:17:07 +0500

    more specs

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,6 +1,4 @@
 class Auction < ApplicationRecord
-	validates_presence_of :title
-	validates_presence_of :description
-	validates_presence_of :start
-	validates_presence_of :end
+	belongs_to :user, optional: true
+	validates_presence_of :title, :description, :start, :end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_183646) do
+ActiveRecord::Schema.define(version: 2021_12_13_191501) do
 
   create_table "auctions", force: :cascade do |t|
     t.datetime "start"
     t.datetime "end"
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
